193fd6c5a7e487cda00ec63d7cc65f973c7134e3
Mike Brasch <mbrasch@mbrasch.local>
2022-01-19 20:40:36 +0000

    .

--- installer/install-nix-darwin.sh
+++ installer/install-nix-darwin.sh
@@ -5,7 +5,7 @@ yes | sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
 
 # neue shell
-source $HOME/.config/zsh/.zshenv
+source $HOME/.config/zsh/zshenv
 
 nix-shell -p nix-info --run "nix-info -m"
 
@@ -14,7 +14,7 @@ sudo rm /etc/nix/nix.conf
 ./result/sw/bin/darwin-rebuild switch --flake .#bootstrap
 
 # neue shell, ab hier regulärer workflow
-source $HOME/.config/zsh/.zshenv
+source $HOME/.config/zsh/zshenv
 
 darwin-rebuild switch --flake .#mbrasch
 
