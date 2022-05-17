8f6982cb5bf146faa653be3fb0563c26d5d787ad
Mike <mike@mbrasch.de>
2022-05-17 15:22:43 +0000

    .

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    nixos.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixos.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
 
     darwin.url = "github:LnL7/nix-darwin";
@@ -13,35 +13,35 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixos-generators.url = "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+    # nixos-generators.url = "github:nix-community/nixos-generators";
+    # nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
-    flake-compat.url = "github:edolstra/flake-compat";
-    flake-compat.flake = false;
-    flake-utils.url = "github:numtide/flake-utils";
-    nixos-shell.url = "github:Mic92/nixos-shell";
+    # flake-compat.url = "github:edolstra/flake-compat";
+    # flake-compat.flake = false;
+    # flake-utils.url = "github:numtide/flake-utils";
+    # nixos-shell.url = "github:Mic92/nixos-shell";
 
-    nur.url = "github:nix-community/NUR";
+    # nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager, nixos-generators, nur, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, nixos, nixos-hardware, darwin, home-manager, ... }@inputs:
     let
       inherit (darwin.lib) darwinSystem;
       inherit (inputs.nixpkgs.lib) attrValues makeOverridable optionalAttrs singleton;
 
-      systems = [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      # systems = [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
+      # forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
 
       nixpkgsConfig = with inputs; rec {
         config = { allowUnfree = true; };
       };
 
-      mkVM = import ./lib/mkvm.nix;
+      # mkVM = import ./lib/mkvm.nix;
 
       # Home Manager configuration shared between all different configurations.
       homeManagerStateVersion = "22.05";
-      homeManagerCommonConfig = { user, userConfig ? ./home + "/users/${user}.nix", ... }: {
-        imports = attrValues self.homeManagerModules ++ [
+      homeManagerCommonConfig = { user, userConfig ? ./home/users + "/${user}.nix", ... }: {
+        imports = [
           userConfig
           ./home
           { home.stateVersion = homeManagerStateVersion; }
@@ -69,7 +69,7 @@
         # Minimal configuration to bootstrap darwin systems
         bootstrap-darwin = makeOverridable darwinSystem {
           system = "aarch64-darwin";
-          modules = [ ./darwin/bootstrap.nix { nixpkgs = nixpkgsConfig; } ];
+          # modules = [ ./darwin/bootstrap.nix { nixpkgs = nixpkgsConfig; } ];
         };
 
         # My primary macOS configuration
@@ -129,17 +129,18 @@
 
       # ----------------------------------------------------------------------------------------------------
 
-#       darwinModules = { };
-#
-#       homeManagerModules = {
-#         #awscli = import ./home/modules/programs/awscli.nix;
-#       };
-#
-#       # `nix develop`
-#       devShell = forAllSystems (system:
-#         let pkgs = nixpkgs.legacyPackages.${system};
-#         in pkgs.mkShell {
-#           nativeBuildInputs = with pkgs; [ rnix-lsp nixpkgs-fmt ];
-#         });
+      # darwinModules = { };
+      #
+      # homeManagerModules = {
+      #  #awscli = import ./home/modules/programs/awscli.nix;
+      # };
+      #
+      # `nix develop`
+      # devShell = forAllSystems (system:
+      #   let pkgs = nixpkgs.legacyPackages.${system};
+      #   in pkgs.mkShell {
+      #     nativeBuildInputs = with pkgs; [ rnix-lsp nixpkgs-fmt ];
+      #   }
+      # );
     };
 }
